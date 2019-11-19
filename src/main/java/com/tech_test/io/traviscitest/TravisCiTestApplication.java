f479602b4881f2c52ed751fdc509390f4c6e3b13
OLETOH_APEDO <aoletoh@gmail.com>
2019-11-18 23:01:33 -0500

    first commit

---
+++ src/main/java/com/tech_test/io/traviscitest/TravisCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.tech_test.io.traviscitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_test/io/traviscitest/TravisCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_test.io.traviscitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
